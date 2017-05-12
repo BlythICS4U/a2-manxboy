fb749ae427ac172592c3f1a8bb77b3becc0650b4
Chris Gallinaro <cgallinaro@ea.com>
2017-05-11 23:47:18 -0400

    Added projects for problems 2 and 3

---
+++ Problem2/src/problem2/Problem2.java
@@ -0,0 +1,16 @@
+package problem2;
+
+/**
+ *
+ * @author cgallinaro
+ */
+public class Problem2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
