a292d1165ef3ecc703fce3b3c4217a9a0152f5d7
Chris Gallinaro <cgallinaro@ea.com>
2017-05-11 23:34:55 -0400

    Added Problem1

---
+++ Problem1/src/problem1/Problem1.java
@@ -0,0 +1,17 @@
+package problem1;
+
+/**
+ *
+ * @author cgallinaro
+ */
+public class Problem1 {
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
